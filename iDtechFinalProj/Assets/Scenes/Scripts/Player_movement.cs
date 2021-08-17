d1f28ea69036091d85df542a5248bb55c0947342
sudo-behappy <69097094+sudo-behappy@users.noreply.github.com>
2021-08-17 23:22:56 +0800

    player
    working on crouch and prone, also going over the obstacles(Unfinished)

--- iDtechFinalProj/Assets/Scenes/Scripts/Player_movement.cs
+++ iDtechFinalProj/Assets/Scenes/Scripts/Player_movement.cs
@@ -6,29 +6,34 @@ public class Player_movement : MonoBehaviour
 {    
     //movement variables
     float forward = 0;
-
-    //
     float rotation = 0;
     float vRotation = 0;
 
     //speed
     public float linearSpeed = 5f;
-    public float angularSpeed = 5f;
+    public float angularSpeed = 20f;
 
-    //forces
-    public float jumpForce = 50f;
+    //playerState
+    int state = 0;
+    string[] stateList = { "Stand", "Crouch", "Prone" };
+    bool canStandUp = true;
 
-    public float lookAngle = 10f;
+    //sensitivity
+    public float lookAngle = 50f;
 
     public Transform camera;
     public Animator Animator;
 
+
+
     Rigidbody rb;
+    Transform ts;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        ts = GetComponent<Transform>();
         rotation = transform.rotation.y;
         vRotation = camera.rotation.x;
     }
@@ -36,7 +41,7 @@ public class Player_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        canStandUp = GetComponentInChildren<headCheck>().canStandUp;
         forward = Input.GetAxis("Vertical");
 
         rotation += Input.GetAxis("Mouse X") * angularSpeed;
@@ -47,13 +52,48 @@ public class Player_movement : MonoBehaviour
         transform.rotation = Quaternion.Euler(0, rotation, 0);
         camera.rotation = Quaternion.Euler(vRotation, rotation, 0);
 
-        if (Input.GetKeyDown(KeyCode.Space) && GetComponentInChildren<jumpTrigger>().isOnGround)
-        {
-            rb.AddForce(Vector3.up * jumpForce);
-            GetComponentInChildren<jumpTrigger>().isOnGround = false;
-        }
-
         Animator.SetFloat("forward", forward);
         Animator.SetFloat("turn", Input.GetAxis("Mouse X"));
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            state += 1;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //movement
+        Vector3 motion = transform.forward * forward * linearSpeed;
+        //motion += transform.forward * left * linearSpeed;
+        motion.y = rb.velocity.y;
+        rb.velocity = motion;
+    }
+
+    void stateUpdate()
+    {
+        state = 1;
+        print(stateList[state]);
+        switch (state % 3)
+        {
+            case 0:
+                //normal standing
+                if ()
+                {
+                    ts.localScale = new Vector3(1, 1.5f, 1);
+                    break;
+                }
+            case 1:
+                //crouch
+                ts.localScale = new Vector3(1, 1, 1);
+                break;
+            case 2:
+                //prone
+                ts.localScale = new Vector3(1, 1.5f, 1);
+                ts.localEulerAngles = new Vector3(90, ts.localEulerAngles.y, 0);
+                break;
+        }
+
+        
     }
 }
