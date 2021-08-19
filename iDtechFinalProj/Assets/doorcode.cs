113bf7f5bf978868e5f0f2a4991cdbfcd412844a
Zachar7C <88907410+Zachar7C@users.noreply.github.com>
2021-08-18 20:25:55 -0700

    well..........
    finnished the map ish working on the blaster animations!

--- iDtechFinalProj/Assets/doorcode.cs
+++ iDtechFinalProj/Assets/doorcode.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class doorcode : MonoBehaviour
 {
+    private Animator ani;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        ani = GetComponent<Animator>();
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "player")
+        {
+            ani.SetBool("letmeopen", true);
+        }
+
+    }
+
+
 
     // Update is called once per frame
     void Update()
