74ff3868c3ea69a7c6b6dbbe248a27cb4c31ec99
Zachar7C <88907410+Zachar7C@users.noreply.github.com>
2021-08-20 14:18:17 -0700

    added blaster stuuf

---
+++ iDtechFinalProj/Assets/Scenes/Scripts/shooter.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator anim;
+    public Camera cam;
+    public float range = 10f;
+    int ammo = 5;
+
+    void Start() {
+
+    }
+    
+        
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+   
+      if (Input.GetMouseButtonDown(0)){
+                ammo --;
+                anim.SetTrigger("PEW");
+ 
+                Vector3 rayOrigin = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f , 0.0f));
+                RaycastHit hit;
+                if (Physics.Raycast(rayOrigin, cam.transform.forward, out hit, range ))
+                {
+                    GameObject hitObject = hit.transform.gameObject;
+                    Debug.Log(hitObject);
+                    if (hitObject.CompareTag("Enemy"))
+                    {
+
+                        //hitObject.GetComponent<enemycontroler>().Damage();
+
+                    }
+
+
+
+                }
+
+
+      }
+      else if (Input.GetKeyDown(KeyCode.R) || ammo == 0)
+        {
+
+           anim.SetTrigger("reload");
+
+
+
+
+        }
+   
+
+
+
+
+
+
+
+
+
+    }
+}
