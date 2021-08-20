436610075a1edbdc30cc30adf9dd851b9c1ba630
Zachar7C <88907410+Zachar7C@users.noreply.github.com>
2021-08-20 15:02:53 -0700

    nice class eh

--- iDtechFinalProj/Assets/Scenes/Scripts/shooter.cs
+++ iDtechFinalProj/Assets/Scenes/Scripts/shooter.cs
@@ -10,59 +10,55 @@ public class shooter : MonoBehaviour
     public float range = 10f;
     int ammo = 5;
 
-    void Start() {
+    void Start()
+    {
 
     }
-    
-        
-    
+
+
+    public void Reload()
+    {
+
+
+
+
+    }
 
     // Update is called once per frame
     void Update()
     {
 
-   
-      if (Input.GetMouseButtonDown(0)){
-                ammo --;
-                anim.SetTrigger("PEW");
- 
-                Vector3 rayOrigin = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f , 0.0f));
-                RaycastHit hit;
-                if (Physics.Raycast(rayOrigin, cam.transform.forward, out hit, range ))
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ammo--;
+            anim.SetTrigger("PEW");
+
+            Vector3 rayOrigin = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+            RaycastHit hit;
+            if (Physics.Raycast(rayOrigin, cam.transform.forward, out hit, range))
+            {
+                GameObject hitObject = hit.transform.gameObject;
+                Debug.Log(hitObject);
+                if (hitObject.CompareTag("Enemy"))
                 {
-                    GameObject hitObject = hit.transform.gameObject;
-                    Debug.Log(hitObject);
-                    if (hitObject.CompareTag("Enemy"))
-                    {
 
-                        //hitObject.GetComponent<enemycontroler>().Damage();
-
-                    }
-
-
+                    //hitObject.GetComponent<enemycontroler>().Damage();
 
                 }
 
 
-      }
-      else if (Input.GetKeyDown(KeyCode.R) || ammo == 0)
-        {
 
-           anim.SetTrigger("reload");
-
-
+            }
 
 
         }
-   
+        else if (Input.GetKeyDown(KeyCode.R) || ammo == 0)
+        {
+
+            anim.SetTrigger("reload");
 
 
-
-
-
-
-
-
-
+        }
     }
 }
